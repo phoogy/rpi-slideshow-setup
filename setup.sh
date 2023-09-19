d76dcc20741ddb6bc57c4592a5ad072295dad967
Phong Au <phoogy21@gmail.com>
2023-09-19 20:17:04 +1000

    Add fbi installation

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@
 if ! command -v rclone &>/dev/null; then
     curl https://rclone.org/install.sh | sudo bash
 fi
+
+# Check if fbi is installed, and install it if not
+if ! command -v fbi &>/dev/null; then
+    sudo apt-get install -y fbi
+fi
