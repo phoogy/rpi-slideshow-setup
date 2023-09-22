d9e643427acb952b1d057e400e336330a78667dd
Phong <phoogy21@gmail.com>
2023-09-23 08:58:17 +1000

    Add function to sync and display

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
+# Function to wait for internet
 wait_for_internet() {
     while ! ping -c 1 google.com &>/dev/null; do
         echo "Waiting for internet connection..."
         sleep 5  # Adjust the interval as needed
     done
+}
+
+# Function to sync and display images
+sync_and_display() {
+    SOURCE_FOLDER="slideshow:"  # Replace with the actual source folder path
+    DESTINATION_PATH="~/slideshow"
+    rclone sync "$SOURCE_FOLDER" "$DESTINATION_PATH"
+
+    # Display all image files in the destination path
+    sudo fbi -T 1 -t 10 -a --noverbose "$DESTINATION_PATH"/*
 }
 
 # Check if rclone is installed, and install it if not
